77d8897e75898a50f5b90fff6fee95aed22bf920
Kat Vicente <katherine.vicente@venmo.com>
2019-01-14 20:17:46 -0600

    javascript

---
+++ app.js
@@ -0,0 +1,16 @@
+// grab hamburger
+var ham = document.querySelector('.hamburger');
+
+
+// grab the nav_mobile
+var mobileMenu = document.querySelector('.nav_mobile');
+
+// create a click event listener
+//hamburger
+ham.addEventListener('click', function (){
+    if(mobileMenu.classList.contains('active')) {
+        mobileMenu.classList.remove('active');
+    } else {
+        mobileMenu.classList.add('active');
+    }
+})
